c69ccb7541a967460f1669277e284f38794a2a19
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-05 19:54:16 +0530

    Adding Method to Pop Last Node

--- src/com/bridgelabz/DataStructureLinkedList.java
+++ src/com/bridgelabz/DataStructureLinkedList.java
@@ -31,15 +31,14 @@ public class DataStructureLinkedList {
             last.next = newNode;
         }
     }
-    //pop method to set head to next node
-    public static void pop(){
-        Node temp =head;
-        if(head == null) {
-            System.out.println("linked list does not have data");
-        }
-        else {
-            head = head.next;
-        }
+    //create pop method make second last node tail node
+    public  void popLastNode(){
+        Node node = head;
+        Node tail = head;
+        while( tail.next.next != null)
+            tail = tail.next;
+        tail.next = null;
+
     }
 
     //display the data to linked list
@@ -66,8 +65,9 @@ public class DataStructureLinkedList {
         display();
         System.out.println("linked list data");
         //pop method point the next node
-        linkedList.pop();
+        linkedList.popLastNode();
         display();
+
 
     }
 }
